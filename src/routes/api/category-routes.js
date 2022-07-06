757e23e1f7b480aae1a7d0e10a4bb2e4d7eee68a
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-07-06 18:30:48 +0100

    post requests finished

--- src/routes/api/category-routes.js
+++ src/routes/api/category-routes.js
@@ -35,7 +35,13 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const createNewCategory = await Category.create(req.body);
+    res.status(200).json(createNewCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
--- src/routes/api/tag-routes.js
+++ src/routes/api/tag-routes.js
@@ -35,7 +35,13 @@ router.get("/:id", async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  try {
+    const createNewTag = await Tag.create(req.body);
+    res.status(200).json(createNewTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
